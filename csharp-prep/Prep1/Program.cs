bc7354ce83a9e7c6e5499b4082b9b5ff01cf3f8b
zcarlsen <zacarlsen@gmail.com>
2024-04-23 15:04:15 -0700

    Completed C# Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("");
+
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+
+        Console.WriteLine("");
+        
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
