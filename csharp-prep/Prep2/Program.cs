61db97268c2846fb5e03dceb1c495111c6fb1ae1
zcarlsen <zacarlsen@gmail.com>
2024-04-25 16:38:48 -0700

    push from mac

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,67 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string gradeLetter;
+        int grade;
+        bool isPassing = true;
+        float remainder;
+        string sign;
+
+
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        grade = int.Parse(userInput);
+
+        if (grade >= 90)
+        {
+            gradeLetter = "A";
+            isPassing = false;
+        }
+        else if (grade >= 80)
+        {
+            gradeLetter = "B";
+            isPassing = true;
+        }
+        else if (grade >= 70)
+        {
+            gradeLetter = "C";
+            isPassing = true;
+        }
+        else if (grade >= 60)
+        {
+            gradeLetter = "D";
+            isPassing = true;
+        }
+        else
+        {
+            gradeLetter = "F";
+            isPassing = false;
+        }
+
+        remainder = grade % 10;
+
+        if (remainder >= 0.7 && gradeLetter != "A" && gradeLetter != "F")
+        {
+            sign = "+";
+        }
+        else if (remainder < 0.3 && gradeLetter != "F")
+        {
+            sign ="-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade: {grade}  Grade Letter: {gradeLetter}{sign}");
+
+        if (isPassing == true)
+        {
+            Console.WriteLine("Congrats, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You can do better!");
+        }
     }
 }
