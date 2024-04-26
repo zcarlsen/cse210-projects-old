63cbbb7457d06a299a2601924a73a944d6649abd
zcarlsen <zacarlsen@gmail.com>
2024-04-26 08:32:26 -0700

    Completed Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Data;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string magic;
+        string guess;
+        int magic_num;
+        int guess_num = 0;
+        int count = 0;
+
+        Console.Write("What is the magic number? ");
+        magic = Console.ReadLine();
+        magic_num = int.Parse(magic);
+
+        while (guess_num != magic_num)
+        {
+            count += 1;
+            Console.Write("What is your guess? ");
+            guess = Console.ReadLine();
+            guess_num = int.Parse(guess);
+
+            if (guess_num > magic_num)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess_num < magic_num)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"You guessed {count} times.");
+            }
+        }
     }
 }
